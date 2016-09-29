77571c9b36594ccbcd5ac11b4c9732dc51c9435d
Jonathon Fitch <lightman1@virginmedia.com>
2016-09-29 15:08:14 +0100

    Update build scripts

--- makekernel.sh
+++ makekernel.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 DEFCONFIG=DarkSense_defconfig
 KERNEL=DarkSense
+TOOLSDIR=tools/tools
 if [  -f $KERNEL ]; then
 	rm $KERNEL
 fi
 if [  -f $DEFCONFIG ]; then
 	rm $DEFCONFIG
+fi
+if [ ! -d "$TOOLSDIR" ]; then
+  mkdir $TOOLSDIR
 fi
 make clean && make mrproper
 export CROSS_COMPILE=~/android/android-ndk-r12b/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin/aarch64-linux-android-
